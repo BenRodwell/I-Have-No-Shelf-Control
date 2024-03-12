f1e30effe53a06d54095cd1f1650134707cab260
Henry Virieux <105490926+hr-virieux@users.noreply.github.com>
2024-03-12 20:09:16 +1100

    Update review.sql
    reviews updated for 5 placeholder books

--- db/review.sql
+++ db/review.sql
@@ -3,4 +3,8 @@ USE book_review_db;
 
 INSERT INTO Reviews (UserID, BookID, ReviewText, Rating, ReviewDate)
 VALUES 
-(1, 1, "It's a book.", 5, '2024-03-12'),
+(1, 1, "It was good", 3, CURDATE()),
+(1, 2, "Better than the anime", 4, CURDATE()),
+(1, 3, "Hur dur 40K ripoff!.", 1, CURDATE()),
+(1, 4, "2spooky4me", 3, CURDATE()),
+(1, 5, "I refuse to watch the movie #JusticeForTomBombadil", 5, CURDATE());
