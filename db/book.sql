e383c88a914364e046ce30b72588fafd8f46b709
Henry Virieux <105490926+hr-virieux@users.noreply.github.com>
2024-03-14 19:03:48 +1100

    Update book.sql
    placeholders updated

--- db/book.sql
+++ db/book.sql
@@ -1,13 +1,13 @@
 -- seed file to contain books
 USE book_review_db;
 
-INSERT INTO Books (Title, Author, Year, Summary)
+INSERT INTO Books (Title, Author, Year, Summary, CoverImage)
 VALUES 
-('Neuromancer', 'William Gibson', 1984, 'A seminal work in the cyberpunk genre, introducing the concept of cyberspace.'),
-('Akira, Vol. 1', 'Katsuhiro Otomo', 1982, 'A dystopian saga set in post-apocalyptic Tokyo.'),
-('Starship Troopers', 'Robert A. Heinlein', 1959, 'A military science fiction novel exploring themes of warfare and civic duty.'),
-('Say Cheese and Die!', 'R.L. Stine', 1992, 'One of the classic books from the Goosebumps series.'),
-('The Fellowship of the Ring', 'J.R.R. Tolkien', 1954, 'The first volume of The Lord of the Rings.');
+('Neuromancer', 'William Gibson', 1984, 'A seminal work in the cyberpunk genre, introducing the concept of cyberspace.', '/images/covers/001.jpg'),
+('Akira, Vol. 1', 'Katsuhiro Otomo', 1982, 'A dystopian saga set in post-apocalyptic Tokyo.', '/images/covers/002.jpg'),
+('Starship Troopers', 'Robert A. Heinlein', 1959, 'A military science fiction novel exploring themes of warfare and civic duty.', '/images/covers/003.jpg'),
+('Say Cheese and Die!', 'R.L. Stine', 1992, 'One of the classic books from the Goosebumps series.', '/images/covers/004.jpg'),
+('The Fellowship of the Ring', 'J.R.R. Tolkien', 1954, 'The first volume of The Lord of the Rings.', '/images/covers/005.jpg');
 
 -- book genre ID
 INSERT INTO BookGenres (BookID, GenreID)
