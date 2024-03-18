017b77049ee932e1dda06d5b4899de0065396499
JLP-55 <jack.v.pekin@gmail.com>
2024-03-18 18:27:53 +1100

    all my work before class trying to add a book to the database from user input, as well as user comments about the book.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
+const addBooks = require("./add_book-routes");
 
+router.use("/addBooks", addBooks);
 router.use('/users', userRoutes);
 
 module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -83,6 +83,34 @@ router.get('/painting/:id', async (req, res) => {
   }
 });
 
+// router.get("/",)
+
+// // post a gallery/genre for the homepage
+// router.post("/", async (req, res) => {
+//   try {
+//     const dbGallerydata = await Painting.create({
+//       // title: req.body.title,
+//       // author: req.body.author,
+//       // year_of_publication: req.body.year,
+//       // filename: req.body.filename,
+//       // summary: req.body.summary,
+//       // gallery_id: req.body.gallery_id,
+//       title: "title",
+//       author: "author",
+//       year_of_publication: "2023",
+//       filename: "filename.file",
+//       summary: "summary",
+//       gallery_id: 4,
+//     });
+//     // console.log(dbGallerydata);
+//     res.status(200).json(dbGallerydata);
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).json(err);
+//   };
+// });
+
+
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
     res.redirect('/');
